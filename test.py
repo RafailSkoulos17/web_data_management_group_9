d8505ea0a24f54b1ff0cd1da48a42f54cbabef19
RafailSkoulos17 <rafail.sk17@hotmail.gr>
2019-05-19 18:55:02 +0200

    Added some functionalities of order service.

--- test.py
+++ test.py
@@ -1,22 +1,26 @@
 from cassandra.cluster import Cluster
 from flask import Flask
+
+from models.order import Order
 from views.api import api
 from cassandra.cqlengine import connection
 from cassandra.cqlengine.management import sync_table
 from models.user import User
+import logging
+
+logging.basicConfig(filename='logger.log', level=logging.ERROR, format='%(asctime)s %(levelname)s %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
 
 
 def create_app():
     app = Flask(__name__)
     app.debug = True
     app.register_blueprint(api)
-
     cluster = Cluster()
     session = cluster.connect()
-    session.execute("DROP KEYSPACE IF EXISTS myfirsttry;")
+    # session.execute("DROP KEYSPACE IF EXISTS myfirsttry;")
     session.execute("CREATE KEYSPACE IF NOT EXISTS myfirsttry WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};")
     cluster.connect(keyspace='myfirsttry')
-
     return app
 
 
@@ -25,4 +29,5 @@ app = create_app()
 if __name__ == '__main__':
     connection.setup(['127.0.0.1'], "cqlengine", protocol_version=3)
     sync_table(User)
+    sync_table(Order)
     app.run()
--- views/api.py
+++ views/api.py
@@ -1,12 +1,18 @@
-from cassandra.cqlengine.query import LWTException
-from flask import Blueprint, Response, render_template, request
-from cassandra.cqlengine import connection
 from models.user import User
+from cassandra.cqlengine.query import LWTException
+from flask import Blueprint, Response
+from cassandra.cqlengine import connection
+from models.order import Order
 import util
 from functools import wraps
 import json
 import flask
 import uuid
+import logging
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 api = Blueprint("api", __name__)
 connection.setup(['127.0.0.1'], "cqlengine")
@@ -18,6 +24,7 @@ def json_api(f):
         result = f(*args, **kwargs)
         json_result = util.to_json(result)
         return Response(response=json_result, status=200, mimetype="application/json")
+
     return decorated_function
 
 
@@ -37,7 +44,7 @@ def create_user():
     data = json.loads(flask.request.data)
     user = User.create(id=uuid.uuid4(), first_name=data["first_name"], last_name=data["last_name"], credit=data["credit"],
                        email=data["email"])
-    # user = Person.create(first_name=request.form["firstname"], last_name=request.form["lastname"])
+    logger.info('Creating user {0} {1} with id={2}'.format(user.first_name, user.last_name, user.id))
     user.save()
     return user.get_data()
 
@@ -106,3 +113,41 @@ def subtract_credit(user_id, amount):
     except ValueError as v_err:
         return {"message": v_err.message}
     return
+
+
+
+@api.route("/orders/create/<uuid:user_id>", methods=["POST"])
+@json_api
+def create_order(user_id):
+    data = json.loads(flask.request.data)
+    users = User.objects.filter(id=user_id)
+    if len(users.all()) != 1:
+        return {"message": "User id is not valid"}
+    else:
+        user = users.all()[0]
+        order_id = uuid.uuid4()
+        order = Order.create(first_name=user["first_name"], last_name=user["last_name"], product=data["product"],
+                             amount=data["amount"], user_id=user["id"], order_id=order_id)
+        order.save()
+        return order.get_data()
+
+
+@api.route("/orders/remove/<uuid:order_id>", methods=["DELETE"])
+@json_api
+def delete_order(order_id):
+    try:
+        Order.objects(order_id=order_id).if_exists().delete()
+    except LWTException:
+        return {"message": "Order cannot be removed"}
+    return {"message": "Order was removed"}
+
+
+@api.route("/orders/find/<uuid:order_id>", methods=["GET"])
+@json_api
+def find_order(order_id):
+    orders = Order.objects.filter(order_id=order_id)
+    if len(orders.all()) != 1:
+        return {"message": "Order id is not valid"}
+    else:
+        order = orders.all()[0]
+    return order.get_data()
