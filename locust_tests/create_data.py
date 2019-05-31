e5861d98cb7b3742b4e0b43ca0deeb6961a1d53a
RafailSkoulos17 <rafail.sk17@hotmail.gr>
2019-06-01 00:49:55 +0200

    Added initial locust implementation

---
+++ locust_tests/create_data.py
@@ -0,0 +1,58 @@
+import json
+import os
+import random
+import string
+import uuid
+
+VOWELS = "aeiou"
+CONSONANTS = "".join(set(string.ascii_lowercase) - set(VOWELS))
+
+
+def generate_word(length):
+    word = ""
+    for i in range(length):
+        if i % 2 == 0:
+            word += random.choice(CONSONANTS)
+        else:
+            word += random.choice(VOWELS)
+    return word
+
+
+def create_users(num_users=100):
+    if os.path.isfile('dummy_data.json'):
+        with open('dummy_data.json', 'r') as f:
+            dummy_data = json.load(f)
+    else:
+        dummy_data = {}
+
+    dummy_data['users'] = []
+    for i in range(num_users):
+        first_name = generate_word(length=10)
+        last_name = generate_word(length=10)
+        credit = random.randint(20, 1000)
+        email = generate_word(5) + '@' + generate_word(5) + '.com'
+        dummy_data['users'] += [[first_name, last_name, email, credit]]
+    with open('dummy_data.json', 'w') as f:
+        json.dump(dummy_data, f, indent=4, separators=(',', ':'))
+
+
+def create_stock(num_products):
+    if os.path.isfile('dummy_data.json'):
+        with open('dummy_data.json', 'r') as f:
+            dummy_data = json.load(f)
+    else:
+        dummy_data = {}
+    dummy_data['stock'] = []
+    for i in range(num_products):
+        product_name = generate_word(10)
+        product_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, product_name))
+        availability = True
+        stock = random.randint(20, 1000)
+        dummy_data['stock'] += [[product_name, product_id, availability, stock]]
+    with open('dummy_data.json', 'w') as f:
+        json.dump(dummy_data, f, indent=4, separators=(',', ':'))
+
+
+if __name__ == '__main__':
+    create_users(num_users=100)
+    create_stock(num_products=1000)
---
+++ locust_tests/locust_testing.py
@@ -0,0 +1,48 @@
+import json
+from random import randint
+
+from locust import HttpLocust, TaskSet, task
+import logging
+
+with open("dummy_data.json", "r") as f:
+    dummy_data = json.load(f)
+
+
+class CreateUsersSteps(TaskSet):
+    def on_start(self):
+        self.email, self.credit, self.first_name, self.last_name = dummy_data['users'][
+            randint(0, len(dummy_data['users']) - 1)]
+        self.product_name, self.product_id, self.availability, self.stock = dummy_data['stock'][
+            randint(0, len(dummy_data['stock']) - 1)]
+
+    # @task
+    # def home(self):
+    #     self.client.get("/")
+    #     logging.info('Created user')
+
+    @task
+    def create_user(self):
+        self.client.post("/users/create/", data=json.dumps({
+            'email': self.email, 'credit': self.credit, 'first_name': self.first_name, 'last_name': self.last_name
+        }), headers={'content-type': 'application/json'})
+        logging.info('Created user %s %s', self.first_name, self.last_name)
+
+    @task
+    def create_stock(self):
+        create_stock_respone = self.client.post("/stock/item/create/", data=json.dumps({
+            'product_name': self.product_name}), headers={'content-type': 'application/json'})
+        # logging.info(create_stock_respone.content)
+        product_id = json.loads(create_stock_respone.content)['product_id']
+        self.client.post("/stock/item/create/{0}/{1}".format(product_id, self.product_name),
+                         headers={'content-type': 'application/json'})
+        logging.info('Created %s products %s ', self.stock, self.product_name)
+
+
+class CreateUsersTest(HttpLocust):
+    task_set = CreateUsersSteps
+    host = "http://3.217.184.15:8083"  # for stock
+    # host = "http://3.217.184.15:8080" #for user
+    sock = None
+
+    def __init__(self):
+        super(CreateUsersTest, self).__init__()
