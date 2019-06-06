0d6ac1378ee6ca2f2b8b4b14c88ae217a481361e
arkajitb <42705169+arkajitb@users.noreply.github.com>
2019-06-06 14:54:40 +0200

    Update

--- aws_scripts/postgres_scripts/stockapp/stock.py
+++ aws_scripts/postgres_scripts/stockapp/stock.py
@@ -1,6 +1,5 @@
-from stock_app import db
+from __init__ import db
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy_views import CreateView, DropView
 
 class Stocks(db.Model):
     __table_name__ = 'stocks'
@@ -8,6 +7,7 @@ class Stocks(db.Model):
     product_name = db.Column(db.String(64), index=False, unique=False, nullable=False)
     stock = db.Column(db.Integer)
     availability = db.Column(db.Boolean())
+    price = db.Column(db.Float(0.0))
 
     def __repr__(self):
             return '<Product %r>' % self.product_name
@@ -17,5 +17,6 @@ class Stocks(db.Model):
                 'product_id': str(self.product_id),
                 'product_name': self.product_name,
                 'stock': self.stock,
-                'availability': self.availability
+                'availability': self.availability,
+                'price': self.price
                }
